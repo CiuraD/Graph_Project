2e3a4a3dc849f584ad25e165ce42f98188fe7b9c
Quizark <staszekmendel1029@gmail.com>
2023-12-27 18:08:15 +0100

    Graph with colors update
    Działa tylko dla metody "teepwalk"

--- embedded/Old functions/teepwalk.py
+++ embedded/Old functions/teepwalk.py
@@ -4,12 +4,49 @@ from gensim.models import Word2Vec
 import time
 
 pathsToFiles = [
+<<<<<<< Updated upstream:embedded/Old functions/teepwalk.py
     r"C:\Users\Admin\Desktop\Graph_Project\Files\aim-100-1_6-no-1.cnf",
     r"C:\Users\Admin\Desktop\Graph_Project\Files\aim-50-1_6-yes1-4.cnf",
     r"C:\Users\Admin\Desktop\Graph_Project\Files\bf0432-007.cnf"
 ]
+=======
+   
+    #Easy
+    r"Z:\Git\Graph_Project\Files\easy\sat\Analiza1-itox_vc1033.cnf",#0
+    r"Z:\Git\Graph_Project\Files\easy\sat\Analiza2-rpoc_xits_15_SAT.cnf",#1
+    r"Z:\Git\Graph_Project\Files\easy\unsat\Analiza1-post-cbmc-aes-ele-noholes.cnf",#2
+    r"Z:\Git\Graph_Project\Files\easy\unsat\Analiza2-een-tip-uns-nusmv-t5.B.cnf",#3
+>>>>>>> Stashed changes:Old functions/teepwalk.py
 
-chosenFile = 1
+    #Medium
+    r"Z:\Git\Graph_Project\Files\medium\sat\Analiza1-vmpc_29.cnf",#4
+    r"Z:\Git\Graph_Project\Files\medium\sat\Analiza2-E02F20.cnf",#5
+    r"Z:\Git\Graph_Project\Files\medium\sat\Analiza3-openstacks-p30_3.085-SAT.cnf",#6
+    r"Z:\Git\Graph_Project\Files\medium\sat\Analiza4-ACG-15-10p1.cnf",#7
+    r"Z:\Git\Graph_Project\Files\medium\unsat\Analiza1-AProVE07-08.cnf",#8
+    r"Z:\Git\Graph_Project\Files\medium\unsat\Analiza2-manol-pipe-g10bid_i.cnf",#9
+    r"Z:\Git\Graph_Project\Files\medium\unsat\Analiza3-grid-strips-grid-y-3.035-NOTKNOWN.cnf",#10
+    r"Z:\Git\Graph_Project\Files\medium\unsat\Analiza4-aaai10-planning-ipc5-pathways-17-step20.cnf",#11
+    #Hard
+    r"Z:\Git\Graph_Project\Files\hard\sat\Analiza1-gss-20-s100.cnf",#12
+    r"Z:\Git\Graph_Project\Files\hard\sat\Analiza2-slp-synthesis-aes-top29.cnf",#13
+    r"Z:\Git\Graph_Project\Files\hard\sat\Analiza3-vmpc_33.cnf",#14
+    r"Z:\Git\Graph_Project\Files\hard\sat\Analiza4-partial-10-17-s.cnf",#15
+    r"Z:\Git\Graph_Project\Files\hard\unsat\Analiza1-rbcl_xits_08_UNSAT.cnf",#16
+    r"Z:\Git\Graph_Project\Files\hard\unsat\Analiza2-6s10.cnf",#17
+    r"Z:\Git\Graph_Project\Files\hard\unsat\Analiza3-smtlib-qfbv-aigs-ext_con_032_008_0256-tseitin.cnf",#18
+    r"Z:\Git\Graph_Project\Files\hard\unsat\Analiza4-hitag2-10-60-0-0xe14721bd199894a-99.cnf",#19
+
+    #Very Hard
+    r"Z:\Git\Graph_Project\Files\very-hard\unsat\Analiza1-openstacks-sequencedstrips-nonadl-nonnegated-os-sequencedstrips-p30_3.025-NOTKNOWN.cnf",#20
+    r"Z:\Git\Graph_Project\Files\very-hard\sat\Analiza1-gss-25-s100.cnf",#21
+
+    #Old
+    r"Z:\Git\Graph_Project\Files\bf0432-007.cnf",#22
+    r"Z:\Git\Graph_Project\Files\aim-50-1_6-yes1-4.cnf",#23
+    r"Z:\Git\Graph_Project\Files\aim-100-1_6-no-1.cnf"#24
+]
+chosenFile = 21
 
 def read_cnf_file(file_path):
     with open(file_path, 'r') as file:
@@ -45,10 +82,7 @@ def create_graph_from_cnf(clauses):
 
 
 
-clauses = read_cnf_file(pathsToFiles[chosenFile])
-graph = create_graph_from_cnf(clauses)
 
-start_time = time.time()  # Record the start time
 
 
 # Parametry DeepWalk
@@ -72,20 +106,22 @@ def generate_walks(graph, num_walks, walk_length):
             walks.append([str(node) for node in walk])
     return walks
 
-# Generowanie wędrówek
-walks = generate_walks(graph, num_walks, walk_length)
+def main():
+    clauses = read_cnf_file(pathsToFiles[chosenFile])
+    graph = create_graph_from_cnf(clauses)
 
-# Utworzenie modelu Word2Vec na podstawie wędrówek
-model = Word2Vec(walks, vector_size=vector_size, window=window_size, sg=1, workers=4)
+    # Calculate the degree of each node (number of connections)
+    node_degrees = dict(graph.degree())
 
-# Rysowanie grafu
-pos = nx.spring_layout(graph)  # Układ wiosennego grafu
-nx.draw(graph, pos, with_labels=True, font_weight='bold', node_color='skyblue', node_size=700, font_size=8)
+    # Sort nodes by degree in descending order
+    sorted_nodes = sorted(node_degrees, key=lambda x: node_degrees[x], reverse=True)
 
-# Dodanie etykiet do węzłów z osadzeniami
-for node, (x, y) in pos.items():
-    plt.text(x, y, f"{node}\n{model.wv[str(node)]}", fontsize=6, ha='center', va='center', bbox=dict(facecolor='white', alpha=0.5))
+    # Normalize degrees to be between 0 and 1
+    min_degree = min(node_degrees.values())
+    max_degree = max(node_degrees.values())
+    normalized_degrees = {node: 1 - ((node_degrees[node] - min_degree) / (max_degree - min_degree)) for node in graph.nodes()}
 
+<<<<<<< Updated upstream:embedded/Old functions/teepwalk.py
 plt.show()
 
 for node in graph.nodes:
@@ -93,3 +129,40 @@ for node in graph.nodes:
         print(f"Node {node} representation:", node_representation)
 elapsed_time = time.time() - start_time  # Calculate elapsed time
 print(f"Graph creation and Node2Vec took {elapsed_time:.2f} seconds.")
+=======
+    # Create a color map based on normalized node degrees
+    color_map = {node: plt.cm.RdYlGn(normalized_degrees[node]) for node in graph.nodes()}
+
+    # Generowanie wędrówek
+    walks = generate_walks(graph, num_walks, walk_length)
+
+    # Utworzenie modelu Word2Vec na podstawie wędrówek
+    model = Word2Vec(walks, vector_size=vector_size, window=window_size, sg=1, workers=4)
+
+    # Rysowanie grafu
+    pos = nx.spring_layout(graph)  # Układ wiosennego grafu
+    nx.draw(graph, pos, with_labels=False, font_weight='bold', node_color=list(color_map.values()), node_size=700, font_size=8)
+
+    # Dodanie legendy
+    norm = plt.Normalize(0, 1)
+    sm = plt.cm.ScalarMappable(cmap=plt.cm.RdYlGn, norm=norm)
+    sm.set_array([])
+
+    # Dodanie kolorowej mapy (colorbar) z dwiema wartościami
+    cbar = plt.colorbar(sm, ticks=[0, 1], label='Normalized Node Degree', ax=plt.gca())
+
+    # Zmiana etykiet na min i max
+    cbar.set_ticklabels(['max', 'min'])
+    
+    plt.show()
+    elapsed_time = time.time() - start_time  # Calculate elapsed time
+    print("Plik:", chosenFile)
+    print(f"Graph creation and Node2Vec took {elapsed_time:.2f} seconds.")
+
+if __name__ == "__main__":
+    print("Metoda DeepWalk")
+    #for chosenFile in range (22):
+    #   for i in range(10):
+    chosenFile = 23
+    main()
+>>>>>>> Stashed changes:Old functions/teepwalk.py
--- embedded/Old functions/wordTOvec.py
+++ embedded/Old functions/wordTOvec.py
@@ -7,12 +7,44 @@ import matplotlib.pyplot as plt
 import time
 
 pathsToFiles = [
+<<<<<<< Updated upstream:embedded/Old functions/wordTOvec.py
     r"C:\Users\Admin\Desktop\Graph_Project\Files\aim-100-1_6-no-1.cnf",
     r"C:\Users\Admin\Desktop\Graph_Project\Files\aim-50-1_6-yes1-4.cnf",
     r"C:\Users\Admin\Desktop\Graph_Project\Files\bf0432-007.cnf"
+=======
+   
+    #Easy
+    r"Z:\Git\Graph_Project\Files\easy\sat\Analiza1-itox_vc1033.cnf",#0
+    r"Z:\Git\Graph_Project\Files\easy\sat\Analiza2-rpoc_xits_15_SAT.cnf",#1
+    r"Z:\Git\Graph_Project\Files\easy\unsat\Analiza1-post-cbmc-aes-ele-noholes.cnf",#2
+    r"Z:\Git\Graph_Project\Files\easy\unsat\Analiza2-een-tip-uns-nusmv-t5.B.cnf",#3
+
+    #Medium
+    r"Z:\Git\Graph_Project\Files\medium\sat\Analiza1-vmpc_29.cnf",#4
+    r"Z:\Git\Graph_Project\Files\medium\sat\Analiza2-E02F20.cnf",#5
+    r"Z:\Git\Graph_Project\Files\medium\sat\Analiza3-openstacks-p30_3.085-SAT.cnf",#6
+    r"Z:\Git\Graph_Project\Files\medium\sat\Analiza4-ACG-15-10p1.cnf",#7
+    r"Z:\Git\Graph_Project\Files\medium\unsat\Analiza1-AProVE07-08.cnf",#8
+    r"Z:\Git\Graph_Project\Files\medium\unsat\Analiza2-manol-pipe-g10bid_i.cnf",#9
+    r"Z:\Git\Graph_Project\Files\medium\unsat\Analiza3-grid-strips-grid-y-3.035-NOTKNOWN.cnf",#10
+    r"Z:\Git\Graph_Project\Files\medium\unsat\Analiza4-aaai10-planning-ipc5-pathways-17-step20.cnf",#11
+    #Hard
+    r"Z:\Git\Graph_Project\Files\hard\sat\Analiza1-gss-20-s100.cnf",#12
+    r"Z:\Git\Graph_Project\Files\hard\sat\Analiza2-slp-synthesis-aes-top29.cnf",#13
+    r"Z:\Git\Graph_Project\Files\hard\sat\Analiza3-vmpc_33.cnf",#14
+    r"Z:\Git\Graph_Project\Files\hard\sat\Analiza4-partial-10-17-s.cnf",#15
+    r"Z:\Git\Graph_Project\Files\hard\unsat\Analiza1-rbcl_xits_08_UNSAT.cnf",#16
+    r"Z:\Git\Graph_Project\Files\hard\unsat\Analiza2-6s10.cnf",#17
+    r"Z:\Git\Graph_Project\Files\hard\unsat\Analiza3-smtlib-qfbv-aigs-ext_con_032_008_0256-tseitin.cnf",#18
+    r"Z:\Git\Graph_Project\Files\hard\unsat\Analiza4-hitag2-10-60-0-0xe14721bd199894a-99.cnf",#19
+
+    #Very Hard
+    r"Z:\Git\Graph_Project\Files\very-hard\unsat\Analiza1-openstacks-sequencedstrips-nonadl-nonnegated-os-sequencedstrips-p30_3.025-NOTKNOWN.cnf",#20
+    r"Z:\Git\Graph_Project\Files\very-hard\sat\Analiza1-gss-25-s100.cnf"#21
+>>>>>>> Stashed changes:Old functions/wordTOvec.py
 ]
 
-chosenFile = 2
+chosenFile = 21
 
 # Załaduj dane CNF z pliku, ignorując linie zaczynające się od 'c' lub 'p cnf'
 def load_cnf(file_path):
@@ -58,20 +90,28 @@ def train_doc2vec_model(tagged_data):
     model.train(tagged_data, total_examples=model.corpus_count, epochs=model.epochs)
     return model
 
-# Przykładowe użycie z dodanym stoperem
-start_time = time.time()
+def main():
+    # Przykładowe użycie z dodanym stoperem
+    start_time = time.time()
 
-cnf_data = load_cnf(pathsToFiles[chosenFile])
-tagged_data = prepare_data(cnf_data)
-model = train_doc2vec_model(tagged_data)
+    cnf_data = load_cnf(pathsToFiles[chosenFile])
+    tagged_data = prepare_data(cnf_data)
+    model = train_doc2vec_model(tagged_data)
 
-# Uzyskaj osadzenia dla grafu CNF
-graph_embedding = model.dv['CNF']
-print("Embedding for CNF:", graph_embedding)
+    # Uzyskaj osadzenia dla grafu CNF
+    graph_embedding = model.dv['CNF']
+    #print("Embedding for CNF:", graph_embedding)
 
-# Rysuj graf CNF
-#draw_cnf_graph(cnf_data)
+    # Rysuj graf CNF
+    #draw_cnf_graph(cnf_data)
 
-end_time = time.time()
-execution_time = end_time - start_time
-print("Execution time: {:.2f} seconds".format(execution_time))
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print("Plik:",chosenFile)
+    print("Podejście:",i)
+    print("Execution time: {:.2f} seconds".format(execution_time))
+
+if __name__ == "__main__":
+    for chosenFile in range (22):
+       for i in range(10):
+            main()
