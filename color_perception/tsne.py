3fb7dd7af33659c4b99f85bf00ea05dd2d4f57b3
iZi11211 <129525902+iZi1211@users.noreply.github.com>
2024-01-15 12:38:01 +0100

    New GUI
    new GUI version

--- color_perception/tsne.py
+++ color_perception/tsne.py
@@ -7,47 +7,7 @@ import time
 import os
 import sys
 
-# Ścieżka bieżąca (aktualny katalog)
-aktualna_sciezka = sys.argv[0]
-aktualna_sciezka = os.path.dirname(os.path.dirname(aktualna_sciezka))
-pathsToFiles = [
-    
-    #Easy
-    r"Files\easy\sat\Analiza1-itox_vc1033.cnf",#0
-    r"Files\easy\sat\Analiza2-rpoc_xits_15_SAT.cnf",#1
-    r"Files\easy\unsat\Analiza1-post-cbmc-aes-ele-noholes.cnf",#2
-    r"Files\easy\unsat\Analiza2-een-tip-uns-nusmv-t5.B.cnf",#3
 
-    #Medium
-    r"Files\medium\sat\Analiza1-vmpc_29.cnf",#4
-    r"Files\medium\sat\Analiza2-E02F20.cnf",#5
-    r"Files\medium\sat\Analiza3-openstacks-p30_3.085-SAT.cnf",#6
-    r"Files\medium\sat\Analiza4-ACG-15-10p1.cnf",#7
-    r"Files\medium\unsat\Analiza1-AProVE07-08.cnf",#8
-    r"Files\medium\unsat\Analiza2-manol-pipe-g10bid_i.cnf",#9
-    r"Files\medium\unsat\Analiza3-grid-strips-grid-y-3.035-NOTKNOWN.cnf",#10
-    r"Files\medium\unsat\Analiza4-aaai10-planning-ipc5-pathways-17-step20.cnf",#11
-
-    #Hard
-    r"Files\hard\sat\Analiza1-gss-20-s100.cnf",#12
-    r"Files\hard\sat\Analiza2-slp-synthesis-aes-top29.cnf",#13
-    r"Files\hard\sat\Analiza3-vmpc_33.cnf",#14
-    r"Files\hard\sat\Analiza4-partial-10-17-s.cnf",#15
-    r"Files\hard\unsat\Analiza1-rbcl_xits_08_UNSAT.cnf",#16
-    r"Files\hard\unsat\Analiza2-6s10.cnf",#17
-    r"Files\hard\unsat\Analiza3-smtlib-qfbv-aigs-ext_con_032_008_0256-tseitin.cnf",#18
-    r"Files\hard\unsat\Analiza4-hitag2-10-60-0-0xe14721bd199894a-99.cnf",#19
-
-    #Very Hard
-    r"Files\very-hard\unsat\Analiza1-openstacks-sequencedstrips-nonadl-nonnegated-os-sequencedstrips-p30_3.025-NOTKNOWN.cnf",#20
-    r"Files\very-hard\sat\Analiza1-gss-25-s100.cnf",#21
-
-    #Old
-    r"Files\bf0432-007.cnf",#22
-    r"Files\aim-50-1_6-yes1-4.cnf",#23
-    r"Files\aim-100-1_6-no-1.cnf"#24
-]
-chosenFile = 0
 
 def read_cnf_file(file_path):
     with open(file_path, 'r') as file:
@@ -76,11 +36,6 @@ def create_graph_from_cnf(clauses):
 
     return G
 
-# Parametry DeepWalk
-walk_length = 10  # Długość pojedynczej wędrówki
-num_walks = 5  # Liczba wędrówek zaczynających się od każdego węzła
-vector_size = 16  # Rozmiar wektora osadzenia
-window_size = 3  # Rozmiar okna w modelu Word2Vec
 
 def generate_walks(graph, num_walks, walk_length):
     walks = []
@@ -96,7 +51,44 @@ def generate_walks(graph, num_walks, walk_length):
             walks.append([str(node) for node in walk])
     return walks
 
-def visualize_deepwalk_results(graph, embedding):
+
+
+
+def tsne(filePath):
+    clauses = read_cnf_file(filePath)
+    graph = create_graph_from_cnf(clauses)
+
+    start_time = time.time()  # Record the start time
+
+    # Parametry DeepWalk
+    walk_length = 10  # Długość pojedynczej wędrówki
+    num_walks = 5  # Liczba wędrówek zaczynających się od każdego węzła
+    vector_size = 16  # Rozmiar wektora osadzenia
+    window_size = 3  # Rozmiar okna w modelu Word2Vec
+
+
+
+    # Calculate the degree of each node (number of connections)
+    node_degrees = dict(graph.degree())
+
+    # Sort nodes by degree in descending order
+    sorted_nodes = sorted(node_degrees, key=lambda x: node_degrees[x], reverse=True)
+
+    # Normalize degrees to be between 0 and 1
+    min_degree = min(node_degrees.values())
+    max_degree = max(node_degrees.values())
+    normalized_degrees = {node: 1 - ((node_degrees[node] - min_degree) / (max_degree - min_degree)) for node in graph.nodes()}
+
+   
+    # Generowanie wędrówek
+    walks = generate_walks(graph, num_walks, walk_length)
+
+    # Utworzenie modelu Word2Vec na podstawie wÄdrÃ³wek
+    model = Word2Vec(walks, vector_size=vector_size, window=window_size, sg=1, workers=4)
+
+    # Pobranie wynikÃ³w DeepWalk (embedding)
+    embedding = np.array([model.wv[str(node)] for node in graph.nodes()])
+
     tsne = TSNE(n_components=2, random_state=42)
     embedding_tsne = tsne.fit_transform(embedding)
 
@@ -127,62 +119,16 @@ def visualize_deepwalk_results(graph, embedding):
         x, y = embedding_tsne[i, 0], embedding_tsne[i, 1]
         plt.text(x, y, label, ha='center', va='center', fontsize=8, bbox=dict(facecolor='red', edgecolor='darkred', boxstyle='circle,pad=0.3'))
 
-    plt.show()
+    elapsed_time = time.time() - start_time  # Calculate elapsed time
+    #print(f"Graph creation and Node2Vec took {elapsed_time:.2f} seconds.")
 
-
-def main():
-    clauses = read_cnf_file(aktualna_sciezka)
-    graph = create_graph_from_cnf(clauses)
-
-    # Calculate the degree of each node (number of connections)
-    node_degrees = dict(graph.degree())
-
-    # Sort nodes by degree in descending order
-    sorted_nodes = sorted(node_degrees, key=lambda x: node_degrees[x], reverse=True)
-
-    # Normalize degrees to be between 0 and 1
-    min_degree = min(node_degrees.values())
-    max_degree = max(node_degrees.values())
-    normalized_degrees = {node: 1 - ((node_degrees[node] - min_degree) / (max_degree - min_degree)) for node in graph.nodes()}
-
-   
-    # Generowanie wędrówek
-    walks = generate_walks(graph, num_walks, walk_length)
-
-    # Utworzenie modelu Word2Vec na podstawie wÄdrÃ³wek
-    model = Word2Vec(walks, vector_size=vector_size, window=window_size, sg=1, workers=4)
-
-    # Pobranie wynikÃ³w DeepWalk (embedding)
-    embedding = np.array([model.wv[str(node)] for node in graph.nodes()])
-
-    # Wizualizacja wynikÃ³w DeepWalk przy uÅ¼yciu t-SNE
-    visualize_deepwalk_results(graph, embedding)
-    # Rysowanie grafu
-    
-
-    
-
-    
-
-   
-    
-    plt.show()
+    results = ""
+    a = 0
 
     for node in graph.nodes:
         node_representation = model.wv.get_vector(str(node))
-        print(f"Node {node} representation:", node_representation)
+        results += f"Node {node} representation: {str(node_representation)}\n"
+        a+=1
     
-    elapsed_time = time.time() - start_time  # Calculate elapsed time
-    print(f"Graph creation and Node2Vec took {elapsed_time:.2f} seconds.")
 
-if __name__ == "__main__":
-    
-    start_time = time.time()  # Start measuring time
-    print("SSSSSSSSSSSSSS")
-    #for chosenFile in range (22):
-    #   for i in range(10):
-    chosenFile = 1
-
-    aktualna_sciezka = aktualna_sciezka + '\\' + pathsToFiles[chosenFile]
-    #print(aktualna_sciezka)
-    main()
+    return graph,results,elapsed_time,a
